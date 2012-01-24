b89a5f7852751ccc4b8b7640e72f1e8c355fc4a2
moon6pence <moon6pence@gmail.com>
2012-01-24 22:10:21 +0900

    Initial Commit

---
+++ libxslt/libxslt.h
@@ -0,0 +1,13 @@
+//
+//  libxslt.h
+//  libxslt
+//
+//  Created by khkim on 12. 1. 24..
+//  Copyright (c) 2012년 chickenjuice.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libxslt : NSObject
+
+@end
